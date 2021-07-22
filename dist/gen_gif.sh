316b5ce1f1865fb40f7ac002d256f79346d3ce79
Antonio <hellocatfood@users.noreply.github.com>
2021-07-22 19:13:12 +0100

    added harbuzz as dependency as it isn't installed on Ubuntu 21.04 by … (#1)
    * added harbuzz as dependency as it isn't installed on Ubuntu 21.04 by default
    
    * gen_gif.sh makes reference to NLI when it should be HOI_unmangled

--- dist/gen_gif.sh
+++ dist/gen_gif.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Requires: HarfBuzz Dev Tools (hb-view), ImageMagick
 
-for i in `seq -f "%04g" 0 10 1000`; do hb-view --variations="0001=$i,0002=$i,0003=$i" NLI_unmangled.ttf -o $i.png --text-file=../gif-text.txt; echo $i; done
+for i in `seq -f "%04g" 0 10 1000`; do hb-view --variations="0001=$i,0002=$i,0003=$i" HOI_unmangled.ttf -o $i.png --text-file=../gif-text.txt; echo $i; done
 convert -delay 1x160 -loop 0 `ls *.png` `ls *.png|sort -r` FontForgeHOI.gif
